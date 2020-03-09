d91504ad1a068bbc2606a903e345bd1acb441482
zjjaxx <2495893564@qq.com>
2020-03-09 20:27:50 +0800

    z-refresh

--- src/components/base/z-refresh.vue
+++ src/components/base/z-refresh.vue
@@ -20,18 +20,18 @@
 </template>
 
 <script>
-const MIN_DISTANCE = 10
+const MIN_DISTANCE = 10;
 
 function getDirection(x, y) {
   if (x > y && x > MIN_DISTANCE) {
-    return 'horizontal'
+    return "horizontal";
   }
 
   if (y > x && y > MIN_DISTANCE) {
-    return 'vertical'
+    return "vertical";
   }
 
-  return ''
+  return "";
 }
 
 export default {
@@ -44,91 +44,68 @@ export default {
       deltaY: 0,
       offsetX: 0,
       offsetY: 0,
-      direction: 'down',
+      direction: "down",
       maxHeight: 0,
-      distance:0
-    }
+      distance: 0
+    };
   },
   computed: {
     c_style() {
-      return { transform: `translate3d(0,${this.distance}px, 0)` }
+      return { transform: `translate3d(0,${this.distance}px, 0)` };
     }
   },
   watch: {},
   methods: {
     touchend() {
-      this.resetTouchStatus()
+      this.resetTouchStatus();
     },
     touchstart(ev) {
-      ev = ev || event
-      this.resetTouchStatus()
-      this.startX = ev.touches[0].clientX
-      this.startY = ev.touches[0].clientY
+      ev = ev || event;
+      this.resetTouchStatus();
+      this.startX = ev.touches[0].clientX;
+      this.startY = ev.touches[0].clientY;
       //   console.log('startX', this.startX, 'startX', this.startY)
     },
     touchmove(ev) {
-      ev = ev || event
-      const touch = ev.touches[0]
-      this.deltaX = touch.clientX - this.startX
-      if (this.offsetY >= this.maxHeight) {
-        if (this.offsetY < this.maxHeight * 2) {
-          this.deltaY = this.maxHeight + (this.offsetY - this.maxHeight) / 2
-          console.log("deltaY",this.deltaY)
-        } else {
-          this.deltaY =
-            this.maxHeight * 1.5 + (this.offsetY - this.maxHeight * 2) / 4
-        }
-      } else {
-        this.deltaY = touch.clientY - this.startY
-      }
-
-      this.offsetX = Math.abs(this.deltaX)
-      this.offsetY = Math.abs(this.deltaY)
+      ev = ev || event;
+      const touch = ev.touches[0];
+      this.deltaX = touch.clientX - this.startX;
+      this.deltaY = touch.clientY - this.startY;
+      this.offsetX = Math.abs(this.deltaX);
+      this.offsetY = Math.abs(this.deltaY);
       this.direction =
-        this.direction || getDirection(this.offsetX, this.offsetY)
-      //   console.log(
-      //     'deltaX',
-      //     this.deltaX,
-      //     'deltaY',
-      //     this.deltaY,
-      //     'offsetX',
-      //     this.offsetX,
-      //     'offsetY',
-      //     this.offsetY,
-      //     'direction',
-      //     this.direction
-      //   )
+        this.direction || getDirection(this.offsetX, this.offsetY);
+      if (this.direction == "vertical"&&this.deltaY>=0) {
+        this.distance=this.ease(this.deltaY);
+      }
     },
-     ease(distance) {
-      const headHeight = +this.headHeight;
-
-      if (distance > headHeight) {
-        if (distance < headHeight * 2) {
-          distance = headHeight + (distance - headHeight) / 2;
+    ease(distance) {
+      if (distance > this.maxHeight) {
+        if (distance < this.maxHeight * 2) {
+          distance = this.maxHeight + (distance - this.maxHeight) / 2;
         } else {
-          distance = headHeight * 1.5 + (distance - headHeight * 2) / 4;
+          distance = this.maxHeight * 1.5 + (distance - this.maxHeight * 2) / 4;
         }
       }
-
       return Math.round(distance);
     },
-
     resetTouchStatus() {
-      this.direction = ''
-      this.deltaX = 0
-      this.deltaY = 0
-      this.offsetX = 0
-      this.offsetY = 0
+      this.direction = "";
+      this.deltaX = 0;
+      this.deltaY = 0;
+      this.offsetX = 0;
+      this.offsetY = 0;
+      this.distance=0
     }
   },
   created() {},
   mounted() {
-    let loadingWrap = this.$el.querySelector('.loading-wrap')
-    this.maxHeight = loadingWrap.offsetHeight
+    let loadingWrap = this.$el.querySelector(".loading-wrap");
+    this.maxHeight = loadingWrap.offsetHeight;
   },
   updated() {}, //生命周期 - 更新之后
   destroyed() {} //生命周期 - 销毁完成
-}
+};
 </script>
 <style lang='less' scoped>
 .z-refresh {
